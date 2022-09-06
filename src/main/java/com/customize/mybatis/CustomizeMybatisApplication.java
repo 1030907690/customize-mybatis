c2f5b2322162e07c42054f7ff3ba6b255b403939
zhouzhongqing <zhouzhongqing>
2022-09-06 13:09:02 +0800

    feat:初始化

---
+++ src/main/java/com/customize/mybatis/CustomizeMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.customize.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomizeMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomizeMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customize/mybatis/CustomizeMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customize.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomizeMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
